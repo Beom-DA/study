bf1512897c2e5a692f1b46c571a5c9bc9c8c620d
Beom-DA <nambi5@naver.com>
2025-07-03 14:31:23 +0900

    Web Crawling sample 1

---
+++ Web_Crawling/crawling_sample.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+with open('HTML_sample.html','r', encoding='utf-8') as f:
+    ex1 = f.read()
+ 
+soup = BeautifulSoup(ex1 , 'html.parser')
+
+print("title 태그에 해당하는 코드 : ", soup.find('title'))
+print("")
+print("p 태그에 해당하는 코드 : ", soup.find('p'))
