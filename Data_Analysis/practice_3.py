0bfab9ab9d6a05004adbfb89a47f5683f74e6c28
Beom-DA <nambi5@naver.com>
2025-07-09 21:35:28 +0900

    practice_4

--- Data_Analysis/practice_3.py
+++ Data_Analysis/practice_3.py
@@ -4,8 +4,37 @@
 
 import pandas as pd
 
-series = pd.Series(['banana', 42])
+'''series = pd.Series(['banana', 42])
 print(series)
 
 s = pd.Series(['Wes McKinney', 'Creator of Pandas'], index=['Person', 'Who'])
-print(s)
+print(s)'''
+
+'''scientists = pd.DataFrame({ 
+    'Name': ['Rosaline Franklin', 'William Gosset'], 
+    'Occupation': ['Chemist', 'Statistician'], 
+    'Born': ['1920-07-25', '1876-06-13'], 
+    'Died': ['1958-04-16', '1937-10-16'], 
+    'Age': [37, 61]}) 
+print(scientists)'''
+
+'''from collections import OrderedDict #순서보장 딕셔너리
+scientists = pd.DataFrame(OrderedDict([ 
+    ('Name', ['Rosaline Franklin', 'William Gosset']),
+    ('Occupation', ['Chemist', 'Statistician']), 
+    ('Born', ['1920-07-25', '1876-06-13']), 
+    ('Died', ['1958-04-16', '1937-10-16']), 
+    ('Age', [37, 61])
+])
+) 
+print(scientists)'''
+
+
+scientists = pd.DataFrame( 
+    data={'Occupation': ['Chemist', 'Statistician'], 
+          'Born': ['1920-07-25', '1876-06-13'], 
+          'Died': ['1958-04-16', '1937-10-16'],
+          'Age': [37, 61]},
+    index=['Rosaline Franklin', 'William Gosset'],
+    columns=['Occupation', 'Born', 'Age', 'Died'])  #columns 열 순서가 유지된다.
+print(scientists)
---
+++ Data_Analysis/practice_4.py
@@ -0,0 +1,5 @@
+# 시리즈의 기초 통계 메소드 사용하기
+# 시리즈의 mean, min, max, std 메소드 사용하기
+# scientists의 age열을 추출한다.
+
+import pandas as pd
