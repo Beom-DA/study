290cbec05ec2ee077c5af2d8244332ecf703bd66
Beom-DA <nambi5@naver.com>
2025-07-15 22:34:26 +0900

    leetcode sql quiz

--- Data_Analysis/SQL/leetcode_8.sql
+++ Data_Analysis/SQL/leetcode_8.sql
@@ -1,7 +1,17 @@
--- https://leetcode.com/problems/customers-who-never-order/description/
+-- https://leetcode.com/problems/department-highest-salary/description/
 
-SELECT name as customers
-    FROM customers AS A
-    left Join orders AS B
-    ON A.id = B.customerid
-    WHERE B.id is NULL;
+-- Write your PostgreSQL query statement below
+
+SELECT D.name Department, E.name Employee, E.salary Salary
+    FROM employee E
+    JOIN department D
+    ON E.departmentid = D.id
+    WHERE (D.name, E.salary) in(
+        SELECT B.name AS Department, MAX(A.salary)
+            FROM employee AS A
+            LEFT JOIN department AS B
+            ON A.departmentID = B.id
+        GROUP BY B.name
+    );
+
+-- WHERE문 안에 in 구문이 있는지 처음 알았다.
