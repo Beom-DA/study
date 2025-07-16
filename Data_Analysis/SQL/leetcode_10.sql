7cde10150ebae696826057608016e6479cf44fc3
Beom-DA <nambi5@naver.com>
2025-07-16 12:12:10 +0900

    https://leetcode.com/problems/rising-temperature/description/

---
+++ Data_Analysis/SQL/leetcode_10.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/delete-duplicate-emails/description/
+
+DELETE FROM Person p1
+USING Person p2
+WHERE p1.email = p2.email
+  AND p1.id > p2.id;
+
+-- DELETE.... USING .... 구문을 처음 봤다.
+-- WHERE문 조건에 해당하는 행이 삭제가 되는데 Person 테이블에서 삭제된다.
