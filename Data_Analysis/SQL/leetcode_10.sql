74a0ec66de7ed998e319b41748844c3bd76b1fd7
Beom-DA <nambi5@naver.com>
2025-07-16 12:12:41 +0900

    https://leetcode.com/problems/delete-duplicate-emails/description/

--- Data_Analysis/SQL/leetcode_10.sql
+++ Data_Analysis/SQL/leetcode_10.sql
@@ -6,4 +6,4 @@ WHERE p1.email = p2.email
   AND p1.id > p2.id;
 
 -- DELETE.... USING .... 구문을 처음 봤다.
--- WHERE문 조건에 해당하는 행이 삭제가 되는데 Person 테이블에서 삭제된다.
+-- WHERE문 조건에 해당하는 행이 삭제가 되는데 Person 테이블에서 삭제된다. 
