8b5e5bf9df9b4e11e1a43a3d1803caba38e38253
Beom-DA <nambi5@naver.com>
2025-07-23 23:21:03 +0900

    data preprocessing

---
+++ Data_Analysis/Python/data_preprocessing/34_선형회귀_그래프에_선형회귀_식과_결정계수_표시하기.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import matplotlib as mpl
+import matplotlib.transforms as transforms
+
+### 선형회귀 그래프에 선형회귀 식과 결정계수 표시하기(Matplotlib & Seaborn)
+
