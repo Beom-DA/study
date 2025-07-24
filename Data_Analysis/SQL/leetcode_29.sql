5529d3bc80fd80d362dd1e8e745d7a48983960bd
Beom-DA <nambi5@naver.com>
2025-07-24 10:27:40 +0900

    https://leetcode.com/problems/swap-salary/

--- Data_Analysis/SQL/leetcode_29.sql
+++ Data_Analysis/SQL/leetcode_29.sql
@@ -1,7 +1,30 @@
--- https://leetcode.com/problems/not-boring-movies/
+-- https://leetcode.com/problems/exchange-seats
 
-SELECT *
-    FROM cinema
-    WHERE id % 2 = 1
-    AND description != 'boring'
-    ORDER BY rating DESC;
+WITH CTE AS(
+    SELECT *,
+        LAG(student) OVER(ORDER BY id),
+        LEAD(student) OVER(ORDER BY id)
+    FROM seat
+)
+
+SELECT id,
+    CASE
+        WHEN id % 2 = 1 
+        AND lead IS NOT NULL THEN lead
+
+        WHEN id % 2 = 0 THEN lag
+
+        ELSE student
+    END as student
+    FROM CTE;
+
+-- 다른 사람의 쿼리
+SELECT(
+  CASE WHEN id % 2 = 1 and  id = (select max(id) from Seat) THEN id
+  WHEN id % 2 = 1 THEN id + 1
+  WHEN id % 2 = 0 THEN id - 1
+  END ) AS id, student 
+FROM Seat
+ORDER BY id
+-- 다음 행을 지정할 수 있는 방법이 있다는걸 처음 알았고
+-- AS id, student 처럼 두 개의 열을 묶어서 가져올 수 있는 것도 처음 알았다.
--- Data_Analysis/SQL/leetcode_30.sql
+++ Data_Analysis/SQL/leetcode_30.sql
@@ -1,2 +1,8 @@
 -- https://leetcode.com/problems/swap-salary
 
+UPDATE salary
+    SET sex =
+    CASE
+        WHEN sex = 'm' THEN 'f'
+        ELSE 'm'
+    END;
