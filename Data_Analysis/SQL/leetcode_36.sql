b3f0f9249645ce2bec449c0c3ed320e602b0a911
Beom-DA <nambi5@naver.com>
2025-07-25 11:58:20 +0900

    https://leetcode.com/problems/sales-analysis-iii/

---
+++ Data_Analysis/SQL/leetcode_36.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/project-employees-i
+
+SELECT project_id, ROUND(AVG(experience_years)::DECIMAL,2) AS average_years
+    FROM project P
+    JOIN employee E
+    USING (employee_id)
+    GROUP BY project_id;  
