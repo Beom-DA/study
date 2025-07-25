b396c0db63f1fa0ebbe2684425c3d21854a09786
Beom-DA <nambi5@naver.com>
2025-07-25 11:58:38 +0900

    https://leetcode.com/problems/sales-analysis-iii/

--- Data_Analysis/SQL/leetcode_36.sql
+++ Data_Analysis/SQL/leetcode_36.sql
@@ -1,7 +1,14 @@
--- https://leetcode.com/problems/project-employees-i
+-- https://leetcode.com/problems/sales-analysis-iii/
 
-SELECT project_id, ROUND(AVG(experience_years)::DECIMAL,2) AS average_years
-    FROM project P
-    JOIN employee E
-    USING (employee_id)
-    GROUP BY project_id;  
+WITH CTE AS(
+    SELECT DISTINCT product_id
+        FROM sales
+        WHERE sale_date < '2019-01-01'
+        OR sale_date > '2019-03-31'
+)
+
+SELECT DISTINCT product_id, product_name
+    FROM product
+    JOIN sales
+    USING (product_id)
+    WHERE product_id NOT IN (SELECT * FROM CTE);
