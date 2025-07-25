f3fb3ef78800e092d82f47d865e6ae0ed4ab3072
Beom-DA <nambi5@naver.com>
2025-07-25 10:53:33 +0900

    https://leetcode.com/problems/project-employees-i

--- Data_Analysis/SQL/leetcode_35.sql
+++ Data_Analysis/SQL/leetcode_35.sql
@@ -4,4 +4,4 @@ SELECT project_id, ROUND(AVG(experience_years)::DECIMAL,2) AS average_years
     FROM project P
     JOIN employee E
     USING (employee_id)
-    GROUP BY project_id;
+    GROUP BY project_id;  
