5310af52131bf11ec6c15ea2c487ec4cd83eac85
Beom-DA <nambi5@naver.com>
2025-07-29 11:23:56 +0900

    https://leetcode.com/problems/students-and-examinations/

--- Data_Analysis/SQL/leetcode_47.sql
+++ Data_Analysis/SQL/leetcode_47.sql
@@ -13,3 +13,7 @@ SELECT CTE.student_id, CTE.student_name, CTE.subject_name, COALESCE(COUNT(E.stud
     ON CTE.student_id = E.student_id and CTE.subject_name = E.subject_name
     GROUP BY CTE.student_id, CTE.student_name, CTE.subject_name
     ORDER BY CTE.student_id, CTE.subject_name;
+
+-- CROSS JOIN
+-- 두 테이블의 모든 행을 서로 조합해서 결과를 생성하는 조인
+-- A 테이블의 행 수 × B 테이블의 행 수 만큼 결과가 나옴
