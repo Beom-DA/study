4820e9473023fff7d9902afb676040423520254f
Beom-DA <nambi5@naver.com>
2025-07-29 13:41:38 +0900

    https://leetcode.com/problems/restaurant-growth/

--- Data_Analysis/SQL/leetcode_48.sql
+++ Data_Analysis/SQL/leetcode_48.sql
@@ -1,3 +1,25 @@
 -- https://leetcode.com/problems/restaurant-growth/
 
 
+WITH CTE AS(
+    SELECT visited_on , SUM(amount) AS amount,
+        LAG(visited_on, 6) OVER(ORDER BY visited_on) AS LAG
+        FROM customer
+        GROUP BY visited_on
+        ORDER BY visited_on
+),
+CTE2 AS(
+    SELECT visited_on,
+        SUM(amount) OVER(ORDER BY visited_on  ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS total_amount
+        FROM CTE
+        GROUP BY visited_on, amount
+)
+
+SELECT visited_on, total_amount AS amount, ROUND(total_amount / 7.0, 2) AS average_amount
+    FROM CTE2
+    JOIN CTE
+    USING (visited_on)
+    WHERE lag IS NOT NULL
+
+
+-- SUM 함수의 OVER() 인자에 이전 몇 행까지의 데이터를 더한다 라는 문법을 알게 됐다.
