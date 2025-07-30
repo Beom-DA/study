30c831cf4cf3578da5692dcd796a59916eb9f301
Beom-DA <nambi5@naver.com>
2025-07-31 02:03:03 +0900

    test

---
+++ Data_Analysis/just_do_it/practice.py
@@ -0,0 +1,58 @@
+import time
+
+import pandas as pd
+
+################## PostgreSql DB에 csv 데이터 넣기 ####################
+'''from sqlalchemy import create_engine
+
+start_time = time.time()
+df_consumption_list = [
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202406_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202407_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202408_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202409_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202410_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202411_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202412_수원시.csv', encoding='euc-kr'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202501_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202502_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202503_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202504_수원시.csv'),
+    pd.read_csv(r'data/소비_데이터/tbsh_gyeonggi_day_202505_수원시.csv'),
+]
+new_df= pd.concat(df_consumption_list, ignore_index=True)
+engine = create_engine('postgresql+pg8000://postgres:1234@localhost:5432/Data', echo=True)
+
+new_df.to_sql('consumption', engine, if_exists='replace', index=False)
+end_time = time.time()
+
+print("걸린 시간 : {:.2f}".format(end_time - start_time))'''
+
+# df = pd.read_csv(r'data/날씨_데이터/수원시_바람_데이터.csv',encoding='euc-kr', skiprows=15)
+# engine = create_engine('postgresql+pg8000://postgres:1234@localhost:5432/Data', echo=True)
+
+# df.to_sql('wind', engine, if_exists='replace', chunksize=1000, index=False)
+
+#######################################################################
+
+
+
+
+df_precipitation['날짜'] = pd.to_datetime(df_precipitation['날짜'])
+df_temperature['날짜'] = pd.to_datetime(df_temperature['날짜'])
+df_humidity['날짜'] = pd.to_datetime(df_humidity['날짜'])
+df_wind['날짜'] = pd.to_datetime(df_wind['날짜'])
+
+## 날씨 데이터의 날짜에 대한 범주를 2024-06-01 ~ 2025-05-30으로 맞춘다.
+df_precipitation = df_precipitation.query('날짜 >= "2024-06-01" & 날짜 <= "2025-05-30"')
+df_temperature = df_temperature.query('날짜 >= "2024-06-01" & 날짜 <= "2025-05-30"')
+df_humidity = df_humidity.query('날짜 >= "2024-06-01" & 날짜 <= "2025-05-30"')
+df_wind = df_wind.query('날짜 >= "2024-06-01" & 날짜 <= "2025-05-30"')
+
+
+## 강수량, 기온, 습도, 바람에 대한 데이터들을 df_weather라는 하나의 데이터프레임에 merge한다.
+df_list = [df_precipitation, df_temperature, df_humidity, df_wind]
+df_weather = df_precipitation
+
+for i in range(len(df_list)-1):
+    df_weather = pd.merge(df_weather,df_list[i+1], on='날짜', how='left')
