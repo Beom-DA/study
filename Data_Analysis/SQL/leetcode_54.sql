a4b10cf3458d753f7bbc00b2478e2b62d4df1e71
Beom-DA <nambi5@naver.com>
2025-07-31 11:02:58 +0900

    https://leetcode.com/problems/group-sold-products-by-the-date/

--- Data_Analysis/SQL/leetcode_54.sql
+++ Data_Analysis/SQL/leetcode_54.sql
@@ -10,3 +10,7 @@ SELECT sell_date, COUNT(*) AS num_sold, STRING_AGG(product, ',' ORDER BY product
     FROM CTE
     GROUP BY sell_date
     ORDER BY sell_date;
+
+
+-- STRING_AGG(column_name, delimiter ORDER BY order)
+-- 같은 행, 같은 열에 원하는 데이터를 원하는 구분자(delimiter)로 이어서 나타내는 방법
