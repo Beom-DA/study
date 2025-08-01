b0023fc92fbbaa92b9d778b14d08fbe9b1a2aa83
Beom-DA <nambi5@naver.com>
2025-08-01 11:47:16 +0900

    https://leetcode.com/problems/average-selling-price/

--- Data_Analysis/SQL/leetcode_46.sql
+++ Data_Analysis/SQL/leetcode_46.sql
@@ -1,2 +1,9 @@
 -- https://leetcode.com/problems/average-selling-price/
 
+
+SELECT P.product_id,
+    COALESCE(ROUND(SUM(price * units) / SUM(units)::DECIMAL, 2), 0) AS average_price
+    FROM prices P
+    LEFT JOIN unitssold U
+    ON P.product_id = U.product_id AND U.purchase_date BETWEEN P.start_date AND P.end_date
+    GROUP BY P.product_id;
---
+++ Data_Analysis/SQL/leetcode_61.sql
@@ -0,0 +1,3 @@
+-- https://leetcode.com/problems/find-students-who-improved/description/
+
+
