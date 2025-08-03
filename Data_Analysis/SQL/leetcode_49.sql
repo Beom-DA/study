50fdb2d48b6a02c35dadbe8094f38fdfd0528074
Beom-DA <nambi5@naver.com>
2025-08-03 12:08:21 +0900

    https://leetcode.com/problems/list-the-products-ordered-in-a-period/description/

--- Data_Analysis/SQL/leetcode_49.sql
+++ Data_Analysis/SQL/leetcode_49.sql
@@ -1,3 +1,16 @@
--- https://leetcode.com/problems/dna-pattern-recognition/
+-- https://leetcode.com/problems/list-the-products-ordered-in-a-period/description/
 
 
+WITH CTE AS(
+    SELECT product_id, SUM(unit)
+        FROM orders
+        WHERE EXTRACT (YEAR FROM order_date) = 2020
+        AND   EXTRACT (MONTH FROM order_date) = 02
+        GROUP BY product_id
+)
+
+SELECT product_name, sum AS unit
+    FROM products
+    JOIN CTE
+    USING (product_id)
+    WHERE sum >= 100;
