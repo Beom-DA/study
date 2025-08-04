dd7f5252ebc84c68f40e3231ce12c3f058b8c483
Beom-DA <nambi5@naver.com>
2025-08-04 12:42:31 +0900

    https://leetcode.com/problems/analyze-subscription-conversion/description/

--- Data_Analysis/SQL/leetcode_65.sql
+++ Data_Analysis/SQL/leetcode_65.sql
@@ -1,3 +1,10 @@
--- https://leetcode.com/problems/analyze-organization-hierarchy/description/
+-- https://leetcode.com/problems/analyze-subscription-conversion/description/
 
 
+SELECT user_id,
+    ROUND((SUM(activity_duration) FILTER (WHERE activity_type = 'free_trial'))::DECIMAL / COUNT(*) FILTER (WHERE activity_type = 'free_trial'), 2) AS trial_avg_duration,
+    ROUND((SUM(activity_duration) FILTER (WHERE activity_type = 'paid'))::DECIMAL / COUNT(*) FILTER (WHERE activity_type = 'paid'), 2) AS paid_avg_duration
+    FROM useractivity
+    GROUP BY user_id
+    HAVING COUNT(*) FILTER (WHERE activity_type = 'paid') > 0
+    ORDER BY user_id;
