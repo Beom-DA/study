81e91ab3bba42811af571b6630ba7674a618be0f
Beom-DA <nambi5@naver.com>
2025-08-06 12:59:27 +0900

    https://leetcode.com/problems/find-covid-recovery-patients/

--- Data_Analysis/SQL/leetcode_69.sql
+++ Data_Analysis/SQL/leetcode_69.sql
@@ -35,3 +35,12 @@ SELECT patient_id, patient_name, age, d2 - d1 AS recovery_time
     ORDER BY recovery_time, patient_name;
     
 
+
+
+-- 다른 사람의 쿼리
+
+SELECT c.patient_id, p.patient_name, p.age, MIN(c1.test_date) - MIN(c.test_date) recovery_time
+FROM covid_tests c INNER JOIN covid_tests c1 ON c.patient_id = c1.patient_id AND c.test_date < c1.test_date AND c.result = 'Positive' AND c1.result = 'Negative'
+INNER JOIN patients p ON c.patient_id = p.patient_id
+GROUP BY c.patient_id, p.patient_name, p.age
+ORDER BY recovery_time, p.patient_name;
