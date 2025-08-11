0f08691f36b82da01f1f9beec95c55dc6a08f096
Beom-DA <nambi5@naver.com>
2025-08-11 11:06:52 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/301650

--- Data_Analysis/SQL/programmers_13.sql
+++ Data_Analysis/SQL/programmers_13.sql
@@ -1,3 +1,20 @@
--- 
+-- https://school.programmers.co.kr/learn/courses/30/lessons/301650
 
 
+WITH RECURSIVE CTE AS(
+    SELECT id, parent_id, 1 AS generation
+        FROM ecoli_data
+        WHERE parent_id IS NULL
+    
+    UNION ALL
+    
+    SELECT E.id AS id, E.parent_id AS parent_id, CTE.generation + 1 AS generation
+        FROM CTE
+        JOIN ecoli_data E
+        ON CTE.id = E.parent_id
+)
+
+SELECT id
+    FROM CTE
+    WHERE generation = 3
+    ORDER BY id;
