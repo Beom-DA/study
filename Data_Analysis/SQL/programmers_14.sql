29acfd48d94b5bc0348fafee1cbacd9ac5dcf3b3
Beom-DA <nambi5@naver.com>
2025-08-11 11:26:51 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/131537

--- Data_Analysis/SQL/programmers_14.sql
+++ Data_Analysis/SQL/programmers_14.sql
@@ -1,3 +1,13 @@
--- 
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131537
 
 
+SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date, product_id, user_id, sales_amount
+    FROM online_sale
+    WHERE EXTRACT(month FROM sales_date) = 3
+    
+UNION ALL
+
+SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date, product_id, NULL AS user_id , sales_amount
+    FROM offline_sale
+    WHERE EXTRACT(month FROM sales_date) = 3
+    ORDER BY sales_date, product_id, user_id;
