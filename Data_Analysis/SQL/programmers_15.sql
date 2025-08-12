1d3a312b1140032d3ca91e9c734ae196c3f12bb8
Beom-DA <nambi5@naver.com>
2025-08-12 11:23:55 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/273711

--- Data_Analysis/SQL/programmers_15.sql
+++ Data_Analysis/SQL/programmers_15.sql
@@ -1,7 +1,7 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/273711
 
 
-select t.ITEM_ID, i.ITEM_NAME, i.RARITY
+select i.ITEM_ID, i.ITEM_NAME, i.RARITY
     from ITEM_INFO i
     join ITEM_TREE t on i.ITEM_ID = t.ITEM_ID
     where PARENT_ITEM_ID in (SELECT ITEM_ID FROM ITEM_INFO WHERE RARITY = 'RARE')
