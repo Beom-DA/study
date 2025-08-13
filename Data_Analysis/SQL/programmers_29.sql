fee242a57dfc9c47451ba23836e9eff7c5eb8738
Beom-DA <nambi5@naver.com>
2025-08-13 13:03:33 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/131532

--- Data_Analysis/SQL/programmers_29.sql
+++ Data_Analysis/SQL/programmers_29.sql
@@ -1,14 +1,12 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/284531
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131532
 
 
 
-WITH CTE AS(
-    SELECT route, ROUND(SUM(d_between_dist),1) AS total_distance,
-        ROUND(AVG(d_between_dist),2) AS average_distance
-        FROM subway_distance
-        GROUP BY route
-        ORDER BY total_distance DESC
-)
-
-SELECT route, CONCAT(total_distance, 'km') AS total_distance, CONCAT(average_distance, 'km') AS average_distance
-    FROM CTE
+SELECT EXTRACT(year FROM sales_date) AS year, EXTRACT(month FROM sales_date) AS month, gender,
+    COUNT(DISTINCT user_id) AS users
+    FROM user_info
+    JOIN online_sale
+    USING (user_id)
+    WHERE gender IS NOT null
+    GROUP BY EXTRACT(year FROM sales_date), EXTRACT(month FROM sales_date), gender
+    ORDER BY year, month, gender;
