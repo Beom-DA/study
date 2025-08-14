ae5e79b887dbafed26332356095cce5842ae9787
Beom-DA <nambi5@naver.com>
2025-08-14 21:28:13 +0900

    ML

--- Data_Analysis/ML/lab_1/basic_linear_algebra.py
+++ Data_Analysis/ML/lab_1/basic_linear_algebra.py
@@ -157,15 +157,17 @@ def matrix_addition(*matrix_variables):
 def matrix_subtraction(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    else:
+        last_matrix = (matrix_variables[0], matrix_addition(*matrix_variables[1:]))
+        return [[x-y for x,y in zip(*matrix)] for matrix in zip(*last_matrix)]
 
-# 실행결과
-matrix_x = [[2, 2], [2, 2]]
-matrix_y = [[2, 5], [2, 1]]
-matrix_z = [[2, 4], [5, 3]]
+# # 실행결과
+# matrix_x = [[2, 2], [2, 2]]
+# matrix_y = [[2, 5], [2, 1]]
+# matrix_z = [[2, 4], [5, 3]]
 
-print (matrix_subtraction(matrix_x, matrix_y)) # Expected value: [[0, -3], [0, 1]]
-print (matrix_subtraction(matrix_x, matrix_y, matrix_z)) # Expected value: [[-2, -7], [-5, -2]]
+# print (matrix_subtraction(matrix_x, matrix_y)) # Expected value: [[0, -3], [0, 1]]
+# print (matrix_subtraction(matrix_x, matrix_y, matrix_z)) # Expected value: [[-2, -7], [-5, -2]]
 
 
 
@@ -180,18 +182,102 @@ print (matrix_subtraction(matrix_x, matrix_y, matrix_z)) # Expected value: [[-2,
 
 
 def matrix_transpose(matrix_variable):
-    return None
+    # col = len(matrix_variable)
+    # row = len(matrix_variable[0])
+    # return [[matrix_variable[i][j] for i in range(col)] for j in range(row)]
+    return [[element for element in row] for row in zip(*matrix_variable)]
+
+# # 실행결과
+# matrix_w = [[2, 5], [1, 1], [2, 2]]
+# print(matrix_transpose(matrix_w))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def scalar_matrix_product(alpha, matrix_variable):
-    return None
+    row = len(matrix_variable)
+    col = len(matrix_variable[0])
+    return [[alpha * matrix_variable[j][i] for i in range(col)] for j in range(row)]
+
+# # 실행결과
+# matrix_x = [[2, 2], [2, 2], [2, 2]]
+# matrix_y = [[2, 5], [2, 1]]
+# matrix_z = [[2, 4], [5, 3]]
+# matrix_w = [[2, 5], [1, 1], [2, 2]]
+
+# print(scalar_matrix_product(3, matrix_x)) #Expected value: [[6, 6], [6, 6], [6, 6]]
+# print(scalar_matrix_product(2, matrix_y)) #Expected value: [[4, 10], [4, 2]]
+# print(scalar_matrix_product(4, matrix_z)) #Expected value: [[8, 16], [20, 12]]
+# print(scalar_matrix_product(3, matrix_w)) #Expected value: [[6, 15], [3, 3], [6, 6]]
+
+
+
+
+
+
+
+
+
+
 
 
 def is_product_availability_matrix(matrix_a, matrix_b):
-    return None
+    col = len(matrix_a[0])
+    row = len(matrix_b)
+    return col == row
+
+# 실행결과
+# matrix_x= [[2, 5], [1, 1]]
+# matrix_y = [[1, 1, 2], [2, 1, 1]]
+# matrix_z = [[2, 4], [5, 3], [1, 3]]
+
+# print(is_product_availability_matrix(matrix_y, matrix_z)) # Expected value: True
+# print(is_product_availability_matrix(matrix_z, matrix_x)) # Expected value: True
+# print(is_product_availability_matrix(matrix_z, matrix_w)) # Expected value: False //matrix_w가없습니다
+# print(is_product_availability_matrix(matrix_x, matrix_x)) # Expected value: True
+
+
+
+
+
+
+
+
+
+
 
 
 def matrix_product(matrix_a, matrix_b):
     if is_product_availability_matrix(matrix_a, matrix_b) == False:
         raise ArithmeticError
-    return None
+    else:
+        # T_matrix_b = matrix_transpose(matrix_b)
+        # product_matrix = [[[a_value,b_value] for b_value in T_matrix_b] for a_value in matrix_a]
+        # return [[sum(x*y for x,y in zip(*col_matrix)) for col_matrix in row_matrix] for row_matrix in product_matrix]
+        return [[sum(a*b for a, b in zip(row_a,column_b)) for column_b in zip(*matrix_b)] for row_a in matrix_a]
+
+# 실행결과
+matrix_x= [[2, 5], [1, 1]]
+matrix_y = [[1, 1, 2], [2, 1, 1]]
+matrix_z = [[2, 4], [5, 3], [1, 3]]
+
+
+# print(matrix_product(matrix_y, matrix_z)) # Expected value: [[9, 13], [10, 14]]
+# print(matrix_product(matrix_z, matrix_x)) # Expected value: [[8, 14], [13, 28], [5, 8]]
+# print(matrix_product(matrix_x, matrix_x)) # Expected value: [[9, 15], [3, 6]]
+# print(matrix_product(matrix_z, matrix_w)) # Expected value: False
