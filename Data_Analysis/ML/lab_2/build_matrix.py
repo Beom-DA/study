d4f06f92f71639d37b01123b5cbc8af0f2e114ba
Beom-DA <nambi5@naver.com>
2025-08-17 17:28:17 +0900

    ML

--- Data_Analysis/ML/lab_2/build_matrix.py
+++ Data_Analysis/ML/lab_2/build_matrix.py
@@ -2,11 +2,23 @@ import numpy as np
 import pandas as pd
 
 
+
+
 def get_rating_matrix(filename, dtype=np.float32):
-    pass
+    df = pd.read_csv(filename).sort_values(['source', 'target'])
+    pivot = pd.pivot_table(data=df, index='source', columns='target', values='rating').fillna(0)
+    arr = np.array(pivot)
+    return arr
 
-
+# filename = r'movie_rating.csv'
+# print(get_rating_matrix(filename))
 
 
 def get_frequent_matrix(filename, dtype=np.float32):
-    pass
+    df = pd.read_csv(filename).sort_values(['source', 'target'])
+    df = df.set_index('source')
+    df = df.groupby('source')['target'].sum()
+    return df
+
+filename = r'1000i.csv'
+print(get_frequent_matrix(filename))
--- Data_Analysis/ML/lab_2/numpy_lab.py
+++ Data_Analysis/ML/lab_2/numpy_lab.py
@@ -8,6 +8,18 @@ def n_size_ndarray_creation(n, dtype=int):
 #print(n_size_ndarray_creation(3))
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 def zero_or_one_or_empty_ndarray(shape, type=0, dtype=int):
     if type == 0:
         X = np.zeros(shape, dtype=int)
@@ -15,32 +27,171 @@ def zero_or_one_or_empty_ndarray(shape, type=0, dtype=int):
         X = np.ones(shape, dtype=int)
     return X
 
-print(zero_or_one_or_empty_ndarray(shape=(2,2), type=1))
+
+#print(zero_or_one_or_empty_ndarray(shape=(2,2), type=1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def change_shape_of_ndarray(X, n_row):
-    pass
+    if n_row == 1 :
+        return X.reshape(n_row,-1).flatten()
+    return X.reshape(n_row,-1)
+# X = np.ones((32,32), dtype=int)
+# print(change_shape_of_ndarray(X, 512))
+
+
+
+
+
+
+
+
+
+
 
 
 def concat_ndarray(X_1, X_2, axis):
-    pass
+    if len(X_1.shape) == 1 :
+        X_1 = X_1.reshape(1,X_1.shape[0])
+    if len(X_2.shape) == 1 :
+        X_2 = X_2.reshape(1,X_2.shape[0])
+    if axis == 0 :
+        if X_1.shape[1] == X_2.shape[1] :
+            return np.concatenate((X_1,X_2), axis=0)
+        else:
+            return False
+    else :
+        if X_1.shape[0] == X_2.shape[0] :
+            return np.concatenate((X_1,X_2), axis=1)
+        else :
+            return False
+
+
+# a = np.array([[1, 2], [3, 4]])
+# b = np.array([[5, 6]])
+# print(concat_ndarray(a, b, 0))
+# print(concat_ndarray(a, b, 1))
+# a = np.array([1, 2])
+# b = np.array([5, 6, 7])
+# print(concat_ndarray(a, b, 1))
+# print(concat_ndarray(a, b, 0))
+
+
+
+
+
+
+
 
 
 def normalize_ndarray(X, axis=99, dtype=np.float32):
-    pass
+    if axis == 0:
+        row_mean = X.mean(axis=0, keepdims = True)
+        row_std = X.std(axis=0, keepdims = True)
+        return (X-row_mean) / row_std
+    elif axis == 1:
+        col_mean = X.mean(axis=1, keepdims = True)
+        col_std = X.std(axis=1, keepdims = True)
+        return (X-col_mean) / col_std
+        return 
+    else :
+        return (X-X.mean()) / X.std()
+
+# X = np.arange(12, dtype=np.float32).reshape(6,2)
+# print(normalize_ndarray(X))
+# print(normalize_ndarray(X, 1))
+# print(normalize_ndarray(X, 0))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def save_ndarray(X, filename="test.npy"):
-    pass
+    np.save(filename, X)
+    return True
+
+X = np.arange(32, dtype=np.float32).reshape(4, -1)
+filename = "test.npy"
+#print(save_ndarray(X, filename)) #test.npy 파일이 생성됨
+
+
+
+
+
+
+
+
+
+
 
 
 def boolean_index(X, condition):
-    pass
+    return X[eval(str('X') + condition)]
+
+# X = np.arange(32, dtype=np.float32).reshape(4, -1)
+# print(boolean_index(X, "== 3"))
+# X = np.arange(32, dtype=np.float32)
+# print(boolean_index(X, "> 6"))
+
+
+
+
+
+
+
+
+
+
+
 
 
 def find_nearest_value(X, target_value):
-    pass
+    arr = np.full((len(X),), target_value)
+    diff = abs(X-arr)
+    index = np.where(diff == diff.min()) 
+    return X[index]
+
+# X = np.random.uniform(0, 1, 100)
+# target_value = 0.3
+# print(find_nearest_value(X, target_value)) # 출력되는 값은 random 하게 바뀜
+
+
+
+
+
+
+
 
 
 def get_n_largest_values(X, n):
-    pass
+    X = np.sort(X)[::-1]
+    return X[:n]
+
+X = np.random.uniform(0, 1, 100)
+print(get_n_largest_values(X, 3))
+print(get_n_largest_values(X, 5))
+# 출력되는 값은 random 하게 바뀜
