40bca497b906ab954f031def57aea0367e2a2e71
Beom-DA <nambi5@naver.com>
2025-08-19 15:38:52 +0900

    https://www.hackerrank.com/challenges/print-prime-numbers/problem?isFullScreen=true

--- Data_Analysis/SQL/hackers_rank_15.sql
+++ Data_Analysis/SQL/hackers_rank_15.sql
@@ -1,16 +1,22 @@
--- https://www.hackerrank.com/challenges/symmetric-pairs/problem?isFullScreen=true
+-- https://www.hackerrank.com/challenges/print-prime-numbers/problem?isFullScreen=true
 
 
 
-SELECT DISTINCT F1.x, F1.y
-    FROM (
-        SELECT *, ROW_NUMBER() OVER() AS rn
-            FROM functions        
-    ) AS F1,
-    (
-        SELECT *, ROW_NUMBER() OVER() AS rn
-            FROM functions    
-    ) AS F2
-    WHERE F1.rn != F2.rn AND
-    (F1.x = F2.y AND F1.y = F2.x) AND F1.x <= F1.y
-    ORDER BY F1.x
+SET cte_max_recursion_depth = 1000;
+
+WITH RECURSIVE CTE AS(
+    SELECT 2 AS init
+    UNION ALL
+    SELECT init + 1
+        FROM CTE
+        WHERE init < 1000
+)
+
+SELECT GROUP_CONCAT(init separator '&')
+    FROM CTE C1
+    WHERE NOT EXISTS(
+        SELECT 1
+            FROM CTE C2
+            WHERE C1.init > C2.init
+            AND C1.init % C2.init = 0
+    )
