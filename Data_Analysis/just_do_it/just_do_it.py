2159180ce8aed6704064cd872114fe3657f9c2a1
Beom-DA <nambi5@naver.com>
2025-09-21 22:51:10 +0900

    just do it

--- Data_Analysis/just_do_it/just_do_it.py
+++ Data_Analysis/just_do_it/just_do_it.py
@@ -349,7 +349,7 @@ print ("RMSLE Value For Random Forest: ",rmsle(y_train, pred, False))
 
 
 # import lightgbm as lgb 
-# from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import mean_squared_error 
 # train_data = lgb.Dataset(x_train, label=y_train)
 # valid_data = lgb.Dataset(x_test, label=y_test)
 
